25e3c17558b123f98fd0bb1db1272524a0deb96f
Benoit Boucounaud <benoit.boucounaud@gmail.com>
2021-12-02 17:20:30 +0100

    add room in mongo when created

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
+import datetime
 
 app = Flask(__name__)
 # enable encryption
@@ -15,19 +16,16 @@ mongo = PyMongo(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     online_users = mongo.db.users.find({"online": True})
-    print(request.method)
     if request.method == 'POST':
         if request.form.get('search') == 'Search/Create':
             # pass
             room_name = request.form.get('roomName')
-            print(room_name)
+            createRoom(room_name)
             return redirect(url_for("room", room_name=room_name))
         else:
             # pass # unknown
             return render_template("index.html",
                                    online_users=online_users)
-    elif request.method == 'GET':
-        print("No Post Back Call")
 
     return render_template("index.html",
                            online_users=online_users)
@@ -46,6 +44,18 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
+
+def createRoom(room_name=None):
+    print(mongo.db.rooms.find({"name": room_name}))
+    if mongo.db.rooms.find({"name": room_name}).count() > 0:
+        pass
+    else:
+        mongo.db.rooms.insert_one({
+            "name": room_name,
+            "creation_date": datetime.datetime.utcnow(),
+            "messages": []
+        })
 
 
 if __name__ == '__main__':
